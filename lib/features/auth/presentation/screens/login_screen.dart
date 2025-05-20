3e5ea92b488c2a8dcb351b33efed993d160aac4a
NiPoltapova <nipoltapova@mail.ru>
2025-05-20 18:48:05 +0300

    переделаал дизайн кнопки входа

--- lib/features/auth/presentation/screens/login_screen.dart
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -14,7 +14,7 @@ class LoginScreen extends StatelessWidget {
             ScaffoldMessenger.of(context).showSnackBar(
               SnackBar(content: Text(state.message)),
             );
-          } 
+          }
           // Добавляем переход при успешной авторизации
           else if (state is AuthAuthenticated) {
             Navigator.pushNamed(context, '/');
@@ -32,7 +32,17 @@ class LoginScreen extends StatelessWidget {
                     onPressed: () {
                       context.read<AuthBloc>().add(LoginRequested());
                     },
-                    child: const Text('Sign in with OAuth'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.purple,
+                      foregroundColor: Colors.white,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 32, vertical: 16),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8)),
+                    ),
+                    child: const Text('Войти',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold)),
                   ),
               ],
             ),
@@ -42,3 +52,63 @@ class LoginScreen extends StatelessWidget {
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:news_feed_neoflex/app_routes.dart';
+// import '../bloc/auth_bloc.dart';
+
+// class LoginScreen extends StatelessWidget {
+//   const LoginScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: BlocListener<AuthBloc, AuthState>(
+//         listener: (context, state) {
+//           if (state is AuthError) {
+//             ScaffoldMessenger.of(context).showSnackBar(
+//               SnackBar(content: Text(state.message)),
+//             );
+//           } else {
+//             // Перенаправляем на NewsFeed после успешной авторизации
+//             Navigator.of(context).pushReplacementNamed(AppRoutes.newsFeed);
+//           }
+//         },
+//         child: Center(
+//           child: Padding(
+//             padding: const EdgeInsets.all(32.0),
+//             child: BlocBuilder<AuthBloc, AuthState>(
+//               builder: (context, state) {
+//                 return Column(
+//                   mainAxisAlignment: MainAxisAlignment.center,
+//                   children: [
+//                     if (state is AuthLoading)
+//                       const CircularProgressIndicator()
+//                     else
+//                       ElevatedButton(
+//                         onPressed: () {
+//                           context.read<AuthBloc>().add(LoginRequested());
+//                         },
+//                         style: ElevatedButton.styleFrom(
+//                           backgroundColor: Colors.purple,
+//                           foregroundColor: Colors.white,
+//                           padding: const EdgeInsets.symmetric(
+//                               horizontal: 32, vertical: 16),
+//                           shape: RoundedRectangleBorder(
+//                               borderRadius: BorderRadius.circular(8)),
+//                         ),
+//                         child: const Text('Войти',
+//                             style: TextStyle(
+//                                 fontSize: 16, fontWeight: FontWeight.bold)),
+//                       ),
+//                   ],
+//                 );
+//               },
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
